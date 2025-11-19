27084a6b6bdbd813c7f43c84ba90c3410baf9511
fakehajabeen <136341482+fakehajabeen@users.noreply.github.com>
2025-11-19 22:22:50 +0530

    Refactor API functions and add CreateNewUser

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,17 +1,53 @@
-import { serve } from "inngest/next";
-import { inngest } from "../../../inngest/client";
-import { CreateNewUser, helloWorld } from "../../../inngest/function";
+mport { inngest } from "./client";
+import { db } from "../configs/db";
+import { eq } from "drizzle-orm";
+//import { generateNotesai } from "../configs/AiModel";  // UNCOMMENT THIS
+
+// ---------------- HELLO WORLD ---------------------
+export const helloWorld = inngest.createFunction(
+  { id: "hello-world" },
+  { event: "test/hello.world" },
+  async ({ event, step }) => {
+    await step.sleep("wait", "1s");
+    return { message: `Hello ${event.data.email}!` };
+  }
+);
 
 
-//Create an API that serves zero functions
-export const { GET, POST, PUT } = serve({
-  client: inngest,
-  functions: [
-    /* your functions will be passed here later! */
-helloWorld,
-CreateNewUser,
-///GenerateNotes
+export const CreateNewUser = inngest.createFunction(
+  
+  { id: "create-user" },
+  { event: "user.create" },
+  async ({ event, step }) => {
+    const result = await step.run(
+      "Check User and create New if not in DB",
+      async () => {
+        // 1. Check if user already exists in array
+        const existingUser = users.find(
+          (u) => u.email === event.data.email
+        );
 
-  ],
-});
+        if (existingUser) {
+          console.log("User already exists:", existingUser);
+          return existingUser;
 
+          
+        }
+
+        // 2. Else, create and add to array
+        const newUser = {
+          id: users.length + 1,
+          name: event.data.name,
+          email: event.data.email,
+        };
+
+        users.push(newUser);
+
+        console.log("Users array:", users);
+
+        return newUser;
+      }
+    );
+
+    return { user: result };
+  });
