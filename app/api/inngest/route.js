65946602620c1aa63b10b97e5946a15b5925b323
fakehajabeen <136341482+fakehajabeen@users.noreply.github.com>
2025-11-19 21:51:18 +0530

    Update route.js

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,8 +1,8 @@
-import { serve } from "inngest/next";
-import { inngest } from "@/inngest/client";
-import { CreateNewUser, helloWorld } from "@/inngest/function";
+// import { serve } from "inngest/next";
+// import { inngest } from "@/inngest/client";
+// import { CreateNewUser, helloWorld } from "@/inngest/function";
 
-export const { GET, POST } = serve({
-  client: inngest,
-  functions: [helloWorld, CreateNewUser],
-});
+// export const { GET, POST } = serve({
+//   client: inngest,
+//   functions: [helloWorld, CreateNewUser],
+// });
