1d0572dff5ea14347e873cbe894d05f1b1702c34
fakehajabeen <136341482+fakehajabeen@users.noreply.github.com>
2025-11-19 20:40:42 +0530

    Remove GenerateNotes import from route.js

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,6 +1,6 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
-import { CreateNewUser, GenerateNotes, helloWorld } from "../../../inngest/function";
+import { CreateNewUser,  helloWorld } from "../../../inngest/function";
 
 
 //Create an API that serves zero functions
