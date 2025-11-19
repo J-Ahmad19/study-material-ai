42187653e022253656c07d8f263bb05400c90f9d
fakehajabeen <136341482+fakehajabeen@users.noreply.github.com>
2025-11-19 22:35:43 +0530

    Refactor API route to use new function structure

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,53 +1,17 @@
-mport { inngest } from "./client";
-import { db } from "../configs/db";
-import { eq } from "drizzle-orm";
-//import { generateNotesai } from "../configs/AiModel";  // UNCOMMENT THIS
-
-// ---------------- HELLO WORLD ---------------------
-export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
-  { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait", "1s");
-    return { message: `Hello ${event.data.email}!` };
-  }
-);
+import { serve } from "inngest/next";
+import { inngest } from "../../../inngest/client";
+import { CreateNewUser, helloWorld } from "../../../inngest/function";
 
 
-export const CreateNewUser = inngest.createFunction(
-  
-  { id: "create-user" },
-  { event: "user.create" },
-  async ({ event, step }) => {
-    const result = await step.run(
-      "Check User and create New if not in DB",
-      async () => {
-        // 1. Check if user already exists in array
-        const existingUser = users.find(
-          (u) => u.email === event.data.email
-        );
+//Create an API that serves zero functions
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+    /* your functions will be passed here later! */
+helloWorld,
+CreateNewUser,
+///GenerateNotes
 
-        if (existingUser) {
-          console.log("User already exists:", existingUser);
-          return existingUser;
+  ],
+});
 
-          
-        }
-
-        // 2. Else, create and add to array
-        const newUser = {
-          id: users.length + 1,
-          name: event.data.name,
-          email: event.data.email,
-        };
-
-        users.push(newUser);
-
-        console.log("Users array:", users);
-
-        return newUser;
-      }
-    );
-
-    return { user: result };
-  });
